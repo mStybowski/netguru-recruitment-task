b523e0d57d1a116034d78c9a230073b3bbe99fb8
Michal Stybowski <mstybowski@gmail.com>
2022-04-30 11:42:31 +0200

    Removed unnecessary comments

--- api-solution/src/models/Movie.js
+++ api-solution/src/models/Movie.js
@@ -1,26 +1,5 @@
 import { query } from "../db/index.js";
 
-// function Movie({ Title, Released, Genre, Director }, userId) {
-//   this.title = Title;
-//   this.released = Released;
-//   this.genre = Genre;
-//   this.director = Director;
-//   this.userId = userId;
-// }
-
-// Movie.prototype.createMovie = async () => {
-//   try {
-//     const { rows } = await query(
-//       `INSERT INTO movies(title, released, genre, director, userId)
-//             VALUES ($1, $2, $3, $4)`,
-//       [this.title, this.released, this.genre, this.director, this.userId]
-//     );
-//     return rows;
-//   } catch (error) {
-//     throw error;
-//   }
-// };
-
 class Movie {
   constructor({ Title, Released, Genre, Director }, userId) {
     this.title = Title;
