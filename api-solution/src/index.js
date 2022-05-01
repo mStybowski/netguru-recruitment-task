11f4372849bd0cbe4e35c1183361aaf9936ce639
Michal Stybowski <mstybowski@gmail.com>
2022-05-01 12:02:52 +0200

    API server core code refactor

---
+++ api-solution/src/index.js
@@ -0,0 +1,6 @@
+import dotenv from "dotenv";
+import { startServer } from "./server.js";
+
+dotenv.config();
+
+startServer();
---
+++ api-solution/src/server.js
@@ -0,0 +1,36 @@
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import mountRoutes from "./routes/index.js";
+import { createMoviesTable } from "./db/index.js";
+
+const { PORT } = process.env;
+
+const server = express();
+server.use(bodyParser.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(cors());
+
+mountRoutes(server);
+
+function startServer() {
+  return new Promise((resolve) =>
+    server.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+      createMoviesTable();
+      resolve();
+    })
+  );
+}
+
+function stopServer() {
+  return new Promise((resolve) =>
+    server.close(() => {
+      console.log("Server stopped");
+      resolve();
+    })
+  );
+}
+
+export { server, startServer, stopServer };
